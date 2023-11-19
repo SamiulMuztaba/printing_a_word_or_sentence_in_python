cc2cfe27b4a908d6c2bf25f1b6a3f33eaf7f4422
Samiul Muztaba <samiulmuztaba@outlook.com>
2023-11-19 17:17:45 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+print('Hello, from here you can print a word/sentence many times you want!')
+
+word = str(input('what is the word/sentence: '))
+print_time = int(input('how many times you want to print: '))
+result = (word + " ") * print_time
+print(result)
+print('you can also copy this words/word/sentence/sentences')
